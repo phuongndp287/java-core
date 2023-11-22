30e8b0dff947bcf05493ce78f993066c6cdaa623
Leo Nguyen <nguyenduyphuong2807@gmail.com>
2023-11-23 00:33:14 +0700

    test: lesson 2 java static and string

---
+++ Lesson2_3/src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    //Convert string number to int value
+    //B1: Chuyển chuỗi số nguyên sang int value. Ví dụ “5646” thành int = 5646.
+
+    //B2: Đảo ngược chuỗi: "Toi La Leo Nguyen" sử dụng vòng lặp hoặc đệ quy.
+    //Có thể sử dụng StringBuilder hoặc StringBuffer để reverse.
+    //Giải thích tại sao lại làm như vậy.
+}
