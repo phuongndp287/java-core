995658bf004fb7a1202f10fbec207db2445e8c2d
TienNguyen0612 <sales08.taybactravel@gmail.com>
2023-11-24 00:11:39 +0700

    StringExercise done

--- Lesson2_3/src/Company.java
+++ Lesson2_3/src/Company.java
@@ -2,5 +2,5 @@ public enum Company {
     // Viet Enum bao gom 3 cong ty CMC GLOBAL, FSOFT, VIETTEL
     CMC_GLOBAL,
     FSOFT,
-//    VIETTEL
+    VIETTEL
 }
--- Lesson2_3/src/StringExercise.java
+++ Lesson2_3/src/StringExercise.java
@@ -1,8 +1,34 @@
 public class StringExercise {
     //Convert string number to int value
     //B1: Chuyển chuỗi số nguyên sang int value. Ví dụ “5646” thành int = 5646.
+    public static void main() {
+        String number = "5646";
+        int i = Integer.parseInt(number);
+        System.out.println(i);
+    }
+
 
     //B2: Đảo ngược chuỗi: "Toi La Leo Nguyen" sử dụng vòng lặp hoặc đệ quy.
     //Có thể sử dụng StringBuilder hoặc StringBuffer để reverse.
     //Giải thích tại sao lại làm như vậy.
+
+    public static String reverseString (String input) {
+        if (input == null) {
+            return input;
+        }
+
+        String reverseString = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverseString = reverseString + input.charAt(i);
+        }
+        return reverseString;
+    }
+
+    public static void main (String[] args) {
+        String input = "Toi La Leo Nguyen";
+        System.out.println("Reverse String: " + reverseString(input));
+
+        StringBuilder stringBuilder = new StringBuilder(input);
+        System.out.println("Reverse String: " + stringBuilder.reverse());
+    }
 }
